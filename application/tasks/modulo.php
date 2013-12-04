f6ae1202aba7f2cc2d4ee3100054baf55c230b73
Ramon Sanches <ramonfilipesanches@gmail.com>
2013-12-04 18:18:49 -0200

    repack artisan
    repacking artisan
    firt version of module task

---
+++ application/tasks/modulo.php
@@ -0,0 +1,15 @@
+<?php
+
+	Class Modulo_Task {
+
+		public function run($arguments)
+		{
+			var_dump($arguments);
+		}
+
+		public function install($arguments)
+		{
+			var_dump($arguments);
+		}
+
+	}
